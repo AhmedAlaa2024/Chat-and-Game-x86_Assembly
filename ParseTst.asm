b2d69dae8b2a75e9d9147065880c1f645a88fd7a
Mohammed Abdulhady <m.abdulhady00@gmail.com>
2021-12-24 17:35:03 +0200

    Parsing Multiple Operands

--- ParseTst.asm
+++ ParseTst.asm
@@ -1,39 +1,72 @@
+;------------------------
+;      PARSER      
+;------------------------
+EXTRN PARSER:FAR
+PUBLIC CMD_MSG, OP1_PROPERTY, OP1_LOCATION, OP2_PROPERTY, OP2_LOCATION, P1_DATA, P1_MEM, P2_DATA, P2_MEM
+
+
 .MODEL SMALL
 .STACK 64
 .DATA
-
-TestString DB 'MOV [F], BX $ '
-TestRes    DB 30 dup (?)
-
-include data.inc
-
-include char.inc
+;========================
+;        Parser Data    
+;========================
+OP1_PROPERTY DB 0
+OP1_LOCATION DW 0
+OP2_PROPERTY DB 0
+OP2_LOCATION DW 0
 include p_data.inc
 
+;==================================
+CMD_BUFF DB 30, ?
+CMD_MSG DB 31 dup('$')
+
+include char.inc
 
 .CODE
-include Parser.inc
+DISPLAY_NEWLINE MACRO
+    MOV AH, 02H
+    
+    MOV DL, 13D ;CARIAGE RETURN
+    INT 21H
+
+    MOV DL, 10D ;LINEFEED
+    INT 21H
+ENDM DISPLAY_NEWLINE
+;include Parser.inc
 MAIN PROC FAR
     ;Initialize Data Segment
     MOV AX, @DATA
     MOV DS, AX
     ;End initialize
 
+    LOOP_INF:
+    MOV DX, OFFSET CMD_BUFF
+    MOV AH, 0AH
+    INT 21H
+    MOV BL, CMD_BUFF[1]
+    MOV BH, 0
+    MOV CMD_MSG[BX], '$'
+    DISPLAY_NEWLINE 
+
+    CALL PARSER
+    DISPLAY_NEWLINE
+    JMP LOOP_INF
     ;===========================
     ;       Test SplitString
     ;===========================
-    SPLIT_STRING TestString, TestRes
-    TOUPPER TestRes, 17
+    ;SPLIT_STRING TestString, TestRes
+    ;TOUPPER TestRes, 17
 
 
     ;===========================
     ;       Test PARSE_CMD
     ;===========================
-    PARSE_CMD TestRes, CMD_ARR, CMD_ARR_SIZE, CMD_FLAG
+    ;PARSE_CMD TestRes, CMD_ARR, CMD_ARR_SIZE, CMD_FLAG
 
-    GET_CMD_OPERANDS CMD_OPERANDS_ARR, CMD_FLAG, OP_FLAGS
+    ;GET_CMD_OPERANDS CMD_OPERANDS_ARR, CMD_FLAG, OP_FLAGS
     
-    PARSE_OPERANDS P1_DATA, TestRes, OP_FLAGS
+    ;PARSE_OPERANDS P1_DATA, TestRes, OP_FLAGS
 
     ;Safely return to OS
     MOV AX, 4C00H
---
+++ Parser_P.asm
@@ -0,0 +1,501 @@
+;JUMPS ;Note this increases memory usage
+LOCALS @@
+PUBLIC PARSER
+EXTRN CMD_MSG:BYTE
+EXTRN OP1_PROPERTY:BYTE
+EXTRN OP1_LOCATION:WORD
+EXTRN OP2_PROPERTY:BYTE
+EXTRN OP2_LOCATION:WORD
+EXTRN P1_DATA:BYTE
+EXTRN P1_MEM:BYTE
+EXTRN P2_DATA:BYTE
+EXTRN P2_MEM:BYTE
+
+.MODEL SMALL
+.DATA
+SPLIT_DATA DB 34 dup(0)
+P_DATA DW ?
+include data.inc
+.CODE
+include char.inc
+
+;=============================================================================
+;Split a string into word slices
+;The seperator character is space ' '
+;INPUT:     '$' Terminated String:                      CMD_MSG
+;OUTPUT:    NULL(ASCII 0) Terminated Array of strings:  SPLIT_DATA
+;
+;Example Usage:
+;   SPLIT_STRING used on '  MOV AX,   DI  $'
+;   Output:
+;       'MOV$AX,$DI$',0
+;
+;   SPLIT_STRING used on '   $'
+;   Output:
+;       0
+;=============================================================================
+SPLIT_STRING PROC NEAR
+    MOV SI, 0   ;CMD_MSG Disp
+    MOV DI, 0   ;SPLIT_DATA Disp
+
+    CMP CMD_MSG[0], ','
+    JNE @@FIRST_CHAR
+
+    CALL PARSE_ERROR
+    RET
+
+    ;Search for first occurrence of a char in CMD_MSG
+    @@FIRST_CHAR:
+    CMP CMD_MSG[SI], ','
+    JNE @@SKIP_COMMA_HANDLING
+    DEC DI
+    @@SKIP_COMMA_HANDLING:
+    CMP CMD_MSG[SI], ' '
+    JNE @@MAIN_LOOP
+    INC SI
+    JMP @@FIRST_CHAR
+
+    @@MAIN_LOOP:
+    CMP CMD_MSG[SI], '$'
+    JE @@RETURN
+
+    ;Check for space
+    CMP CMD_MSG[SI], ' '
+    ;Non space characted detected
+    JNE @@INSERT
+    
+    ;'$' Terminate current slice
+    MOV SPLIT_DATA[DI], '$'
+    INC DI
+    JMP @@FIRST_CHAR ;Skip to next occurrence of a non space character
+    
+
+    ;Insert Character into SPLIT_DATA
+    @@INSERT:
+    MOV AL, CMD_MSG[SI]
+    CMP AL, ','
+    JNE @@NOT_COMMA
+    MOV SPLIT_DATA[DI], '$'
+    INC DI
+    MOV SPLIT_DATA[DI], ','
+    INC DI
+    MOV SPLIT_DATA[DI], '$'
+    INC DI
+    INC SI
+    JMP @@FIRST_CHAR
+    @@NOT_COMMA:
+    MOV SPLIT_DATA[DI], AL
+    INC DI
+    
+    ;Advance to next iteration
+    INC SI
+    JMP @@MAIN_LOOP
+
+    @@RETURN:
+    CMP DI, 0
+    JE @@SKIP
+
+    CMP SPLIT_DATA[DI] - 1, '$'
+    JE @@SKIP
+
+    MOV SPLIT_DATA[DI], '$'
+    INC DI
+    @@SKIP:
+    ;Array of strings is terminated by NULL (ASCII 0)
+    MOV SPLIT_DATA[DI], 0
+    
+    RET
+SPLIT_STRING ENDP
+
+PARSE_ERROR PROC NEAR
+    MOV AH, 02
+    MOV DL, 'E'
+    INT 21H
+    RET
+PARSE_ERROR ENDP
+
+
+PARSE_VALID PROC NEAR
+    MOV AH, 02
+    MOV DL, 'V'
+    INT 21H
+    RET
+PARSE_VALID ENDP
+
+PARSE_CMD PROC NEAR
+    MOV DI, 0
+    MOV SI, 0
+    MOV CMD_FLAG, 0
+
+    @@MAIN_LOOP:
+    ;Reached end of CMD_ARR (No cmd matched)
+    CMP DI, CMD_ARR_SIZE
+    JE @@INVALID
+    
+    ;Check if SPLIT_DATA ended
+    CMP SPLIT_DATA[SI], '$'
+    ;Check if current cmd also ended
+    JE @@CHECK_VALID
+
+    MOV DL, SPLIT_DATA[SI]
+    CMP DL, CMD_ARR[DI]
+    ;Cmd mismatch
+    JNE @@SKIPCMD
+
+    INC SI
+    INC DI
+    JMP @@MAIN_LOOP
+
+    @@CHECK_VALID:
+    CMP CMD_ARR[DI], '$'
+    ;Cmd matched!
+    JE @@VALID
+    ;Cmd mismatch
+    JMP @@SKIPCMD
+
+    ;Skip to next cmd in CMD_ARR
+    @@SKIPCMD:
+    CMP CMD_ARR[DI], '$'
+    JE @@FINALLY
+
+    INC DI
+    JMP @@SKIPCMD
+
+    @@FINALLY:
+    INC DI
+    ;Reset to first char in SPLIT_DATA
+    MOV SI, 0
+    INC CMD_FLAG
+    JMP @@MAIN_LOOP
+
+    @@INVALID:
+    CALL PARSE_ERROR
+    RET
+
+    @@VALID:
+    CALL PARSE_VALID
+    RET
+PARSE_CMD ENDP
+
+PARSE_OPERAND PROC NEAR
+    ;CHECK MEMORY MODE
+    @@MEM_CHK:
+    CMP SPLIT_DATA[SI], '['
+    JNE @@REG_CHK
+
+    ;REG INDIRECT CHECK
+    INC SI
+    ;CX Contain
+    MOV CX, OFFSET [DI][6]
+
+    MOV BP, WORD PTR SPLIT_DATA[SI]
+
+    CMP BP, WORD PTR BX_STR
+    JE @@REG_INDIRECT_VALID
+    ADD CX, 2
+    CMP BP, WORD PTR SI_STR
+    JE @@REG_INDIRECT_VALID
+    ADD CX, 2
+    CMP BP, WORD PTR DI_STR
+    JE @@REG_INDIRECT_VALID
+    ADD CX, 2
+    CMP BP, WORD PTR BP_STR
+    JE @@REG_INDIRECT_VALID  
+
+    ;DIRECT ADRESSING CHECK
+    MOV BH, 0
+    MOV BL, SPLIT_DATA[SI]
+    
+    ;Check if digit
+    CMP BL, '0'
+    JL @@INVALIDHEX
+
+    CMP BL, '9'
+    JLE @@IS_DIGIT
+
+    ;Check if A-F
+    CMP BL, 'A'
+    JL @@INVALIDHEX
+
+    CMP BL, 'F'
+    JG @@INVALIDHEX
+
+    JMP @@IS_HEXCHAR
+    @@INVALIDHEX:
+    CALL PARSE_ERROR
+    RET
+
+    @@IS_HEXCHAR:
+    SUB BL, 55D ;Get decimal value of hex char
+    JMP @@GET_P_MEM
+
+    @@IS_DIGIT:
+    SUB BL, '0' ;Get decimal value of digit
+    
+    @@GET_P_MEM:
+    MOV CX, OFFSET [DI][BX][16]
+    JMP @@DIRECT_ADDR_VALID
+
+    @@REG_INDIRECT_VALID:
+    INC SI
+    @@DIRECT_ADDR_VALID:
+    INC SI
+
+    CMP SPLIT_DATA[SI], ']'
+    JNE @@INVALID_MEM
+
+    INC SI
+
+    CMP SPLIT_DATA[SI], '$'
+    JNE @@INVALID_MEM
+
+    MOV BX, DX
+    MOV [BX][1], CX
+    MOV BYTE PTR [BX], 00111000B
+    INC SI
+    JMP @@VALID
+
+    @@INVALID_MEM:
+    CALL PARSE_ERROR
+    RET
+    
+    @@REG_CHK:
+    CMP SPLIT_DATA[SI][2], '$'
+    JNE @@IMED_CHK
+    MOV BX, 0
+    MOV BP, WORD PTR SPLIT_DATA[SI]
+    @@REG_CHK_LOOP:
+    CMP BX, REG_STR_ARR_SIZE
+    JE @@IMED_CHK
+
+    CMP BP, WORD PTR REG_STR_ARR[BX]
+    JE @@REG_VALID
+    ADD BX, 3
+    JMP @@REG_CHK_LOOP
+
+    @@REG_VALID:
+    MOV CL, 01000000B ;Set property type
+    MOV AL, REG_STR_ARR[BX][2]
+    MOV AH, AL
+    SHR AH, 3
+    AND AH, 00011000B
+    OR  CL, AH ;Set property size
+    MOV BX, DX
+    MOV BYTE PTR [BX], CL
+    AND AL, 0FH ;AL now contains p_data location disp
+    MOV AH, 0
+    MOV BX, AX
+    MOV CX, OFFSET [DI][BX]
+    MOV BX, DX
+    MOV WORD PTR [BX][1], CX ;Set OP Location
+    ADD SI, 3
+    JMP @@VALID
+
+
+    @@IMED_CHK:
+    MOV AX, 0   
+    MOV BH, 0
+    MOV CL, 0
+
+    ;CHECK IF IMMMEDIATE BEGINS WITH DIGIT
+    MOV BL, SPLIT_DATA[SI]
+    CMP BL, '0'
+    JL @@INVALID
+
+    CMP BL, '9'
+    JG @@INVALID
+
+    @@SKIP_LEADING_ZEROS:
+    CMP SPLIT_DATA[SI], '$'
+    JE @@IMED_CHK_LOOP
+    CMP SPLIT_DATA[SI], '0'
+    JNE @@IMED_CHK_LOOP
+    INC SI
+    JMP @@SKIP_LEADING_ZEROS
+
+    @@IMED_CHK_LOOP:
+    MOV BL, SPLIT_DATA[SI]
+    CMP BL, '$'
+    JE @@EXIT_IMMED_LOOP
+
+    CMP CL, 4
+    JGE @@INVALID
+
+    ;Check if digit
+    CMP BL, '0'
+    JL @@INVALID
+
+    CMP BL, '9'
+    JLE @@IS_DIGIT_IMMED
+
+    ;Check if A-F
+    CMP BL, 'A'
+    JL @@INVALID
+
+    CMP BL, 'F'
+    JG @@INVALID
+
+
+    @@IS_HEX_IMMED:
+    SUB BL, 55D ;Get decimal value of hex char
+    JMP @@CALC_IMMED
+
+    @@IS_DIGIT_IMMED:
+    SUB BL, '0' ;Get decimal value of digit
+
+    @@CALC_IMMED:
+    SHL AX, 4
+    ADD AX, BX
+
+    INC CL
+    INC SI
+    JMP @@IMED_CHK_LOOP
+    @@EXIT_IMMED_LOOP:
+    INC SI
+    ;AX NOW CONTAINS IMMEDIATE VALUE
+    MOV [DI][32], AX
+    MOV BP, OFFSET[DI][32]
+    ;BX = OFFSET OP_PROPERTY
+    ;BX + 1 = OFFSET OP_LOCATION
+    MOV BX, DX
+    MOV [BX][1], BP
+    
+    MOV BYTE PTR [BX], 10010000B
+    CMP AX, 0FFH
+    JG @@VALID
+
+    MOV BYTE PTR [BX], 10001000B
+    JMP @@VALID
+
+    @@INVALID:
+    CALL PARSE_ERROR
+    RET
+
+    @@VALID:
+    CALL PARSE_VALID
+    RET
+PARSE_OPERAND ENDP
+
+PARSE_CMD_OPERANDS PROC NEAR
+    MOV SI, 0
+    MOV DI, P_DATA
+
+    @@SKIPWORD:
+    CMP SPLIT_DATA[SI], '$'
+    JE @@FINALLY
+
+    INC SI
+    JMP @@SKIPWORD
+
+    @@FINALLY:
+    INC SI
+
+    ;CHECK IF FIRST OPERAND IS NEEDED
+    MOV CL, OP_FLAGS
+    ;CL != 0 IF First Operand is needed
+    AND CL, 10000000B
+
+    CMP CL, 0
+    JE NEAR PTR @@FINAL_CHECK
+
+    ;CMP SPLIT_DATA[SI], 0
+    ;JNE @@NOT_END_OF_DATA
+    ;CALL PARSE_ERROR
+    ;RET
+    ;@@NOT_END_OF_DATA:
+
+    ;Has first operand
+    MOV DX, OFFSET OP1_PROPERTY
+    CALL PARSE_OPERAND
+
+    ;Check if first operand is valid
+    MOV CL, OP_FLAGS
+    MOV DL, OP1_PROPERTY
+    AND CL, 01110000B ;get supported modes
+    AND DL, 11100000B
+
+    SHL CL, 1
+    AND CL, DL
+    CMP CL, 0 ;If CL == 0 then mode mismatch
+    JE @@INVALID 
+
+
+    ;CHECK IF SECOND OPERAND IS NEEDED
+    MOV CL, OP_FLAGS
+    ;CL != 0 IF SECOND Operand is needed
+    AND CL, 00001000B
+
+    CMP CL, 0
+    JE @@FINAL_CHECK
+
+    ;Check for comma
+    CMP WORD PTR SPLIT_DATA[SI], 242CH ;',$'
+    JNE @@INVALID
+    ADD SI, 2
+
+    ;Has second operand
+    MOV DX, OFFSET OP2_PROPERTY
+    CALL PARSE_OPERAND 
+
+    ;Check if second operand is valid
+    MOV CL, OP_FLAGS
+    MOV DL, OP2_PROPERTY
+    AND CL, 00000111B ;get supported modes
+    AND DL, 11100000B
+
+    SHR DL, 5
+    AND CL, DL
+    CMP CL, 0 ;If CL == 0 then mode mismatch
+    JE @@INVALID 
+
+    ;MEM TO MEM Check
+    MOV CL, OP1_PROPERTY
+    MOV DL, OP2_PROPERTY
+    AND CL, 00100000B ;Get mem mode
+    AND DL, 00100000B ;Get mem mode
+    AND CL, DL
+    CMP CL, 0 ;CL != 0 if both are mem
+    JNE @@INVALID
+
+    ;Size Mismatch check
+    MOV CL, OP1_PROPERTY
+    MOV DL, OP2_PROPERTY
+    AND CL, 00011000B ;Get sizes
+    AND DL, 00011000B ;Get sizes
+
+    AND CL, DL
+    CMP CL, 0 ;CL == 0 if size mismatch
+    JE @@INVALID
+
+    @@FINAL_CHECK:
+    CMP SPLIT_DATA[SI], 0
+    JNE @@INVALID
+    RET
+
+    @@INVALID:
+    CALL PARSE_ERROR
+    RET 
+PARSE_CMD_OPERANDS ENDP
+
+PARSER PROC FAR
+    ;Initialize Data Segment
+    MOV AX, @DATA
+    MOV DS, AX
+    ;End initialize
+    
+
+    CALL SPLIT_STRING
+    TOUPPER SPLIT_DATA, 30
+
+    CALL PARSE_CMD
+
+    ;Get command operands
+    MOV BX, OFFSET CMD_OPERANDS_ARR
+    MOV AL, CMD_FLAG
+    XLAT
+    MOV OP_FLAGS, AL
+
+    CALL PARSE_CMD_OPERANDS
+    RET
+PARSER ENDP
+END PARSER
