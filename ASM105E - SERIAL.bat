7634519a43b14acf894d41f9012a7724fe107943
mahmed amer <mahmedamer887@gmail.com>
2022-01-07 21:47:22 +0200

    config adn main screen

---
+++ ASM105E - SERIAL.bat
@@ -0,0 +1,6 @@
+start "port1" /B "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" -conf ASM105E-PORT1.conf
+ping localhost
+ping localhost
+ping localhost
+ping localhost 
+start "port2" /B "C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe" -conf ASM105E-PORT2.conf
